95509b1985991a97666f8b99720e2cab2d09d42a
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-17 19:02:05 -0700

    write model test for pets_on_application

--- spec/models/application_pet_spec.rb
+++ spec/models/application_pet_spec.rb
@@ -1,8 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationPet, type: :model do
+
   describe "relationship" do
     it {should belong_to :pet}
     it {should belong_to :application_form}
+  end
+
+  describe "class methods" do
+    it "#pets_on_application" do
+
+      dfl = Shelter.create!(name: 'Dumb Friends League', address: '191 Yuma Street', city: 'Denver', state: 'CO', zip: 80223)
+
+      chippy = dfl.pets.create!(image: "IMG_0337.JPG", name: "chippy", approximate_age: 1, description: "dumb", sex: 1)
+
+      app_1 = ApplicationForm.create!(name:"jake",
+                                       street_address:"123 st",
+                                       city:"Mead",
+                                       state:"CO",
+                                       zip_code:80235,
+                                       description:"i want puppy"
+                                        )
+
+
+      ApplicationPet.create!(application_form: app_1, pet: chippy)
+
+      expected = [chippy]
+
+      expect(ApplicationPet.pets_on_application(app_1.id)[0].pet_id).to eq(chippy.id)
+    end
   end
 end
