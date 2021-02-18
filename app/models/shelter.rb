2c895a1e4565fea21afe61103a378a1c6f1284eb
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-18 16:50:54 -0700

    Add method for sorting shelters

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,3 +1,7 @@
 class Shelter < ApplicationRecord
   has_many :pets
+
+  def self.reverse_order_by_name
+    Shelter.find_by_sql("SELECT shelters.* FROM shelters ORDER BY shelters.name DESC")
+  end
 end
