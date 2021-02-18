636c536d77aaff5ef7cadb3149c2e9e8e11aba8b
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-17 18:19:07 -0700

    Add class methods to look up pets on application

--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -1,4 +1,22 @@
 class ApplicationPet < ApplicationRecord
   belongs_to :application_form
   belongs_to :pet
+
+  def self.pets_on_application(application_id)
+    where("application_form_id = ?", application_id)
+  end
+
+  def self.pet_on_app(pet_id, app_id)
+    where("pet_id = ? AND application_form_id = ?", pet_id, app_id)
+  end
+
+  def self.approved?(pet_id, app_id)
+    application = ApplicationForm.find(app_id)
+    pets = ApplicationPet.where("pet_id = ? AND application_form_id = ?", pet_id, app_id)
+    if pets.where("accepted = ?", false).count >= 1
+      application.update(accepted: false)
+    elsif pets.where("accepted = ?", true).count == pets.count
+      application.update(accepted: true)
+    end
+  end
 end
