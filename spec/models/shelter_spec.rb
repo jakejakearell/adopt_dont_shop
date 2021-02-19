74bd7928358dadcbe9762ac19fc3f3fe2facd75a
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-18 17:03:43 -0700

    Make assertions that actually test  class method

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -7,19 +7,16 @@ describe Shelter, type: :model do
 
   describe 'class methods' do
     it "#reverse_order_by_name" do
-
-
       lhs = Shelter.create!(name: 'Zippy Humane Society', address: '9595 Nelson Road', city: 'Longmont', state: 'CO', zip: 80501)
       dfl = Shelter.create!(name: 'Arbies Friends League', address: '191 Yuma Street', city: 'Denver', state: 'CO', zip: 80223)
       dfl = Shelter.create!(name: 'Dumb Friends League', address: '191 Yuma Street', city: 'Denver', state: 'CO', zip: 80223)
 
-      within "#shelter-0" do
-        expect(page).to have_content("Zippy")
-      end
+      shelters = Shelter.reverse_order_by_name
 
-      within "#shelter-2" do
-        expect(page).to have_content("Arbies")
-      end
+      expect(Shelter.reverse_order_by_name.first.name).to eq("Zippy Humane Society")
+      expect(Shelter.reverse_order_by_name.last.name).to eq("Arbies Friends League")
+
+
     end
   end
 end
